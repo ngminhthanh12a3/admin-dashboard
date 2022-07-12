6b045c7095e0c6bb86f4fb247663a2faccbcd16f
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-07-12 03:24:16 +0000

    test

--- api_server/models/DeviceInfo.js
+++ api_server/models/DeviceInfo.js
@@ -26,7 +26,7 @@ var deviceInfoSchema = new Schema({
   version: { type: Number, default: 1.0 },
   ota_type: { type: String, default: "Local" },
   devicetype: { type: String, default: "ESP32" },
-  ota_upload_time: { type: Number, default: -1 },
+  ota_upload_time: { type: Number, default: NaN },
 });
 
 const DeviceInfo = mongoose.model("DeviceInfo", deviceInfoSchema);
